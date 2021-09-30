9c4ace6a17ec6b951005bd7a53f3eaa6742f039d
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-09-30 17:13:35 -0300

    refactor: query wikibase's api directly
    Using SPARQL to connect to the API gave us the possibility to query for
    more data once the concepts were matched. However, it also meant to
    limit the amount of queries per minute (solved by waiting 2 extra
    minutes after each error...).
    
    Wikibase-api provides a very simple way to achieve our goal without
    having to worry about the timeout exception (as far as I could test).
    
    We could still use SPARQL to get some specific piece of information, but
    it doesn't feel very appropriate for this task.

--- gen-ontoscen/src/wikilink.py
+++ gen-ontoscen/src/wikilink.py
@@ -1,8 +1,11 @@
+from functools import reduce
 from .ontoscen import Ontoscen
-from SPARQLWrapper import SPARQLWrapper, JSON
-from time import sleep
-from rdflib import URIRef, Graph, Literal
+from wikibase_api import Wikibase
+from rdflib import URIRef, Graph, Literal, Namespace
 from rdflib.namespace import OWL, RDFS
+
+
+SCHEMA = Namespace("https://schema.org/")
 
 
 class Wikilink:
@@ -15,6 +18,7 @@ class Wikilink:
 
     def __init__(self, limit: int = 10):
         self.LIMIT = limit
+        self.WB = Wikibase()
 
     def enrich(self, graph: Ontoscen) -> Ontoscen:
         """Enrich the resources and actors of an Ontoscen graph with
@@ -27,11 +31,11 @@ class Wikilink:
             graph (Ontoscen): an Ontoscen graph linked with Wikidata.
         """
 
-        output = graph
-        for subject in graph.get_resources() + graph.get_actors():
-            output = self._enrich_subject(output, subject)
-
-        return output
+        return reduce(
+            self._enrich_subject,
+            graph.get_resources() + graph.get_actors(),
+            graph,
+        )
 
     def _enrich_subject(self, ontoscen: Ontoscen, subject: URIRef) -> Ontoscen:
 
@@ -42,44 +46,74 @@ class Wikilink:
         if not label:
             return ontoscen
 
-        results: list = self._query_wikidata(label.toPython())
+        results: list = self._query(label.toPython())
 
         if not results:
             return ontoscen
 
-        chosen_result: dict = self._take_input(results, subject)
+        chosen_result: dict = self._take_input(results, subject, label)
 
         if not chosen_result:
             return ontoscen
 
         ontoscen.add(
-            (subject, OWL.sameAs, URIRef(chosen_result["item"]["value"]))
+            (subject, OWL.sameAs, URIRef(chosen_result["concepturi"]))
         )
-        ontoscen.add(
-            (subject, RDFS.label, Literal(chosen_result["label"]["value"]))
-        )
+        ontoscen.add((subject, RDFS.label, Literal(chosen_result["label"])))
+
+        if "description" in chosen_result.keys():
+            ontoscen.add(
+                (
+                    subject,
+                    SCHEMA.description,
+                    Literal(chosen_result["description"]),
+                )
+            )
 
         return ontoscen
 
-    def _take_input(self, options: list[dict], subject: URIRef) -> dict:
+    def _take_input(
+        self, options: list[dict], subject: URIRef, subject_label: str
+    ) -> dict:
         print(
-            f"The subject '{subject}' has the following wikidata matches.",
-            "Choose the most precise option, or press Enter to skip.",
+            "--------------------------------------------------------------------------------------------------------------------",
+            f"The subject '{subject_label}' ({subject}) matches the following wikidata concepts.",
+            "Select the most suitable option, or press Enter to skip.",
+            "",
             sep="\n",
         )
 
         index: int = 0
         for option in options:
             index += 1
-            external_item: str = option["item"]["value"]
-            external_label: str = option["label"]["value"]
-            print(f"{index}) {external_label}: {external_item}")
+            item: str = option["concepturi"]
+            label: str = option["label"]
 
+            description: str = ""
+            if "description" in option.keys():
+                description: str = option["description"]
+
+            print(
+                f"{index}) {label}:",
+                f"ConceptUri: {item}",
+                sep="\n",
+            )
+
+            if description:
+                print(f"Description: {description}")
+
+            print("")
+
+        selection = input("Select: ")
         try:
-            selection: int = int(input("Select: "))
-            return options[selection - 1]
+            return options[int(selection) - 1]
         except:
             return {}
+
+    def _query(self, item_label: str) -> list[dict]:
+        return self.WB.entity.search(item_label, "en", limit=self.LIMIT)[
+            "search"
+        ]
 
     def _is_enriched(self, subject: URIRef, graph: Graph) -> bool:
         return (subject, OWL.sameAs, None) in graph or (
@@ -87,52 +121,3 @@ class Wikilink:
             OWL.sameAs,
             subject,
         ) in graph
-
-    def _query_wikidata(self, name: str) -> list[dict]:
-        query = """
-            SELECT ?item ?label WHERE {
-              SERVICE wikibase:mwapi {
-                bd:serviceParam wikibase:api "EntitySearch".
-                bd:serviceParam wikibase:endpoint "www.wikidata.org".
-                bd:serviceParam mwapi:search "%(subject)s".
-                bd:serviceParam mwapi:language "en".
-                ?item wikibase:apiOutputItem mwapi:item.
-              }
-            ?item rdfs:label ?label. FILTER( LANG(?label)="en" )
-            }
-            LIMIT %(limit)s
-            """ % {
-            "subject": name,
-            "limit": self.LIMIT,
-        }
-
-        return self._request_query("https://query.wikidata.org/sparql", query)[
-            "results"
-        ]["bindings"]
-
-    def _request_query(self, url: str, query: str):
-        sparql = SPARQLWrapper(url, returnFormat=JSON)
-        sparql.setQuery(query)
-        try:
-            return sparql.queryAndConvert()
-        except:
-            print("Too many queries were made... please wait a few minutes...")
-            sleep(120)
-            return sparql.queryAndConvert()
-
-    def _get_subject(self, graph: Graph) -> URIRef:
-        query = graph.query(
-            """
-            SELECT DISTINCT ?subject
-            WHERE {
-              ?subject ?predicate ?object
-            } LIMIT 1
-            """
-        )
-        for subject in query:
-            if subject is None:
-                raise Exception("Subject not found.")
-
-            return subject[0]
-
-        raise Exception("Subject not found.")
